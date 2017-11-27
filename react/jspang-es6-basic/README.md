4ec702c9cfa1b279833743ef4e44e00cb31454e4
Android-Xiaobai <1006430902@qq.com>
2017-11-27 23:03:38 +0800

    ES6对象和Symbol

--- react/jspang-es6-basic/README.md
+++ react/jspang-es6-basic/README.md
@@ -392,3 +392,126 @@ console.log(arr.join('|'));
 console.log(arr.toString());
 //jspang,技术胖,ES6
 ```
+
+## ES6的对象
+
+### 对象的赋值
+
+  - 将变量直接赋给对象,变量名就是对象的属性名，变量的值就是属性的值
+```javascript
+let name = 'jspang';
+let skill = 'web';
+var obj1 = {name,skill};
+console.log(obj1);
+//{name: "jspang", skill: "web"}
+```
+
+  - 将变量的值作为对象的属性名
+```javascript
+let key = 'skill';
+var obj2 = {
+  [key] : 'web'
+}
+console.log(obj2);
+{skill: "web"}
+```
+
+### 对象方法
+```javascript
+var obj3={
+    add:function(a,b){
+        return a+b;
+    }
+}
+console.log(obj3.add(1,2));  //3
+```
+  - Object.is(a,b) 对象比较(只能用来比较对象的属性值)
+```javascript
+//传统方法比较对象的值
+var obj01 = {name:'jspang'};
+var obj02 = {name:'jspang'};
+console.log('obj01.name === obj02.name',obj01.name === obj02.name);//true
+console.log('obj01 === obj02',obj01 === obj02);//false
+```
+```javascript
+//Object.is(a,b)
+console.log('Object.is(obj01.name,obj02.name)',Object.is(obj01.name,obj02.name)); //true
+console.log('Object.is(obj01,obj02)',Object.is(obj01,obj02)); //false
+```
+
+  - 区分=== 和 is方法的区别。
+```javascript
+console.log(+0 === -0);  //true
+console.log(NaN === NaN ); //false
+console.log(Object.is(+0,-0)); //false
+console.log(Object.is(NaN,NaN)); //true
+
+console.log(+0 === -0);  //true
+console.log(NaN === NaN ); //false
+console.log(Object.is(+0,-0)); //false
+console.log(Object.is(NaN,NaN)); //true
+```
+  小技巧 : ===为同值相等，is()为严格相等。
+
+### Object.assign( )合并对象(同数组合并)
+```javascript
+let a = {a :'jspang'};
+let b = {b : '技术派'};
+let c = {c : 'web'};
+let d = Object.assign(a,b,c);
+console.log(d);
+```
+## Symbol 全局标记
+
+### Symbol声明
+```javascript
+var f = Symbol();
+console.log(typeof(f));
+```
+### Symbol打印
+```javascript
+var g = Symbol('jspang');
+console.log(g);  //红色
+console.log(g.toString()); //黑色
+```
+###  使用Symbol构建对象的key值
+
+```javascript
+var jspang = Symbol();
+var obj={
+    [jspang]:'技术胖'
+}
+console.log(obj[jspang]);
+obj[jspang]='web';
+console.log(obj[jspang]);
+// 技术胖
+// web
+```
+ *有人就会问了，这和不声明jspang=Symbol();有什么区别呢？*
+
+### Symbol对象中属性的的保护作用
+
+  - 没有保护的写法
+```javascript
+var person ={name:'jspang',skill:'web',age:18};
+for (let item in person){
+    console.log(person[item]);
+}
+// jspang
+// web
+// 18
+```
+  - 有保护的写法
+```javascript
+let person2={name:'jspang',skill:'web'};
+let age=Symbol();
+person2[age]=18;
+for (let item in person2){
+    console.log(person2[item]);
+    // jspang
+    // web
+}
+console.log(person2);
+// {name: "jspang", skill: "web", Symbol(): 18}
+```
+ *我们看到person2对象的age的属性名被隐藏了，但是它的值仍然可以被修改。*
--- react/jspang-es6-basic/dist/index.js
+++ react/jspang-es6-basic/dist/index.js
@@ -1,76 +1,113 @@
 'use strict';
 
-//day02的第二部分今天学的有点多，将其分开写更加清晰
-//ES6中的函数和数组补漏
-//对象的函数解构
-var json = {
-  a: 'jspang',
-  b: '技术胖'
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+//day03
+//ES6的对象
+
+//对象的赋值
+//将变量直接赋给对象,变量名就是对象的属性名，变量的值就是属性的值
+var name = 'jspang';
+var skill = 'web';
+var obj1 = { name: name, skill: skill };
+console.log(obj1);
+//{name: "jspang", skill: "web"}
+
+
+//讲变量的值作为对象的属性名
+var key = 'skill';
+var obj2 = _defineProperty({}, key, 'web');
+console.log(obj2);
+{
+    skill: "web";
+}
+
+//对象方法
+
+var obj3 = {
+    add: function add(a, b) {
+        return a + b;
+    }
 };
+console.log(obj3.add(1, 2)); //3
 
-function fun1(a) {
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'jspang';
+//Object.is(a,b) 对象比较(只能用来比较对象的属性值)
+//传统方法比较对象的值
 
-  console.log(a, b);
+var obj01 = { name: 'jspang' };
+var obj02 = { name: 'jspang' };
+console.log('obj01.name === obj02.name', obj01.name === obj02.name); //true
+console.log('obj01 === obj02', obj01 === obj02); //false
+
+//Object.is(a,b)
+console.log('Object.is(obj01.name,obj02.name)', Object.is(obj01.name, obj02.name)); //true
+console.log('Object.is(obj01,obj02)', Object.is(obj01, obj02)); //false
+
+//区分=== 和 is方法的区别。
+
+console.log(+0 === -0); //true
+console.log(NaN === NaN); //false
+console.log(Object.is(+0, -0)); //false
+console.log(Object.is(NaN, NaN)); //true
+
+console.log(+0 === -0); //true
+console.log(NaN === NaN); //false
+console.log(Object.is(+0, -0)); //false
+console.log(Object.is(NaN, NaN)); //true
+
+//===为同值相等，is()为严格相等。
+
+//Object.assign( )合并对象(同数组合并)
+var a = { a: 'jspang' };
+var b = { b: '技术派' };
+var c = { c: 'web' };
+var d = Object.assign(a, b, c);
+console.log(d);
+
+//Symbol 全局标记
+
+//Symbol声明
+var f = Symbol();
+console.log(typeof f === 'undefined' ? 'undefined' : _typeof(f));
+
+var g = Symbol('jspang');
+console.log(g); //红色
+console.log(g.toString()); //黑色
+
+// 使用Symbol构建对象的key值
+
+
+var jspang = Symbol();
+var obj = _defineProperty({}, jspang, '技术胖');
+console.log(obj);
+console.log(obj[jspang]);
+obj[jspang] = 'web';
+console.log(obj);
+console.log(obj[jspang]);
+// 技术胖
+// web
+// 有人就会问了，这和不声明jspang=Symbol();有什么区别呢？
+// Symbol对象中属性的的保护作用
+
+// 没有保护的写法
+var person = { name: 'jspang', skill: 'web', age: 18 };
+
+for (var item in person) {
+    console.log(person[item]);
 }
-fun1(json);
-// {a: "jspang", b: "技术胖"} "jspang"
+// jspang
+// web
+// 18
 
-// 数组的函数解构
-
-var arr = ['jspang', '技术胖', 'ES6'];
-
-function fun2(a, b, c) {
-  console.log(a, b, c);
+var person2 = { name: 'jspang', skill: 'web' };
+var age = Symbol();
+person2[age] = 18;
+for (var _item in person2) {
+    console.log(person2[_item]);
+    // jspang
+    // web
 }
-fun2.apply(undefined, arr);
-// jspang 技术胖 ES6
-
-// in的用法
-// 对象
-
-console.log('a' in json); //true
-
-// 数组
-
-var arr01 = [,,,,,,];
-console.log(arr01.length); //6
-console.log(0 in arr01); //false
-console.log(0 in arr); //true
-
-
-//数组的遍历方法
-// forEach
-// filter
-// some
-// map
-// forEach 传值时val在前index在后
-arr.forEach(function (val, index) {
-  return console.log(index, val);
-});
-//0 "jspang"
-//1 "技术胖"
-//2 "ES6"
-arr.filter(function (x) {
-  return console.log(x);
-});
-//jspang
-//技术胖
-//ES6
-arr.some(function (x) {
-  return console.log(x);
-});
-//jspang
-//技术胖
-//ES6
-console.log(arr.map(function (x) {
-  return 'web';
-}));
-//(3) ["web", "web", "web"]
-
-
-//数组转换字符串
-//join()
-//toString()
-console.log(arr.join('|'));
-console.log(arr.toString());
+console.log(person2);
+// {name: "jspang", skill: "web", Symbol(): 18}
