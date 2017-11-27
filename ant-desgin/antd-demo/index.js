307ab2a41dc9946c17787dbf8db462d5ccbc841b
yookoo <1006430902@qq.com>
2017-11-27 08:19:45 +0800

    添加ant-desgin

---
+++ ant-desgin/antd-demo/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Button } from 'antd';
+
+import { DatePicker, message } from 'antd';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+    };
+  }
+  handleChange(date) {
+    message.info('您选择的日期是: ' + date.toString());
+    this.setState({ date });
+  }
+  render() {
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={value => this.handleChange(value)} />
+        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+class Btn extends React.Component {
+
+  render() {
+    return (
+     <div>
+	    <Button type="primary">Primary</Button>
+	    <Button type="dashed">Dashed</Button>
+	    <Button type="danger">Danger</Button>
+	    <Button type="default"> Default </Button>
+	 </div>
+    );
+  }
+}
+
+ReactDOM.render(<Btn />, document.getElementById('root2'));
